f2c5c261784f93377db54cfca30c866009c44b89
Minmin20112 <138598512+Minmin20112@users.noreply.github.com>
2023-07-04 22:36:16 +0700

    Update Consecutive.c

--- Consecutive.c
+++ Consecutive.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-
+Tran Tuan Minh
 Welcome to GDB Online.
   GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
   C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
